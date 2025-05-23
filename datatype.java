dd168da5f163c4cecc48d81c6b99ad76df1028b8
LEELA MANOHAR <leelamanoharmadivada@gmail.com>
2025-05-23 22:31:13 +0530

    datatype.java

---
+++ datatype.java
@@ -0,0 +1,18 @@
+class Datatypes
+{
+    public static void main(String args[])
+    {
+        //interger datatypes
+        byte  b = 127; 
+        short s = 654;  
+        int i =467;
+        long l = 78965l;
+        //f;oating point datatypes
+        double d =4.5;
+        //character
+        char c = 'a';
+        //boolean
+        boolean bo = true;
+
+     }
+}
