f8df8816508273a2e24b966648fc0e8f0fea33dc
LEELA MANOHAR <leelamanoharmadivada@gmail.com>
2025-05-24 21:09:37 +0530

    Add files via upload

---
+++ explicit_type_convertion.java
@@ -0,0 +1,10 @@
+class convertion
+{
+    public static void main(String args[])
+    {
+        //implicit type convertion
+        int b = 257;
+        byte a =(byte)b;
+        System.out.println(a);
+    }
+}
---
+++ implicit_type_convertion.java
@@ -0,0 +1,10 @@
+class convertion
+{
+    public static void main(String args[])
+    {
+        //implicit type convertion
+        byte b = 34;
+        int a = b;
+        System.out.println(a);
+    }
+}
